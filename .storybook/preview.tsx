6fe4642b11b16bc40a559c05e06142af28bceb06
Roman Chagovcev <sunduck248@gmail.com>
2024-01-17 13:40:44 +0100

    added docs and fixed Readme

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,8 @@
 import { useEffect } from 'react';
-import type { Decorator, Preview } from '@storybook/react';
+import type { Decorator, Preview, StoryFn } from '@storybook/react';
+import { Provider } from 'react-redux';
+import { store } from '@store';
+import { withRouter } from 'storybook-addon-react-router-v6';
 
 import '../src/assets/themes.scss';
 
@@ -7,13 +10,22 @@ const withThemeDecorator: Decorator = (Story, context) => {
   useEffect(() => {
     const { theme } = context.globals;
 
+    // @ts-ignore
     document.documentElement.dataset.theme = theme;
   }, [context]);
 
   return <Story />;
 };
 
-export const decorators = [withThemeDecorator];
+export const withStore = (StoryFn: StoryFn) => {
+  return (
+    <Provider store={store}>
+      <StoryFn />
+    </Provider>
+  );
+};
+
+export const decorators = [withThemeDecorator, withStore, withRouter];
 
 export const globalTypes = {
   theme: {
@@ -29,10 +41,6 @@ export const globalTypes = {
       showName: true,
     },
   },
-};
-
-export const parameters = {
-  layout: 'centered',
 };
 
 const preview: Preview = {};
---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,14 @@
+import { Header } from '@components';
+
+const meta = {
+  title: 'components/Header',
+  component: Header,
+  argTypes: {},
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Playground = {
+  args: {},
+};
